1ecc731172e1f6cb258115143fddeaa3ef806389
Giorgio Borelli <casaborelli@gmail.com>
2015-05-02 16:12:26 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ setup(
         'zope.component',
         'pygeoif > 0.2',
         'collective.geo.geographer >= 2.0',
-        'collective.geo.mapwidget >= 2.1.1',
-        'collective.z3cform.mapwidget >= 2.0'
+        'collective.geo.mapwidget >= 2.2',
+        'collective.z3cform.mapwidget >= 2.1'
     ],
     extras_require={
         'test': [
